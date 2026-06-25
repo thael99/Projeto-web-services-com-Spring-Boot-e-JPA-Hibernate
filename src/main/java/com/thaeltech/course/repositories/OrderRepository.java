54416c62b1eb9de5790c28bc5d8d314a68671ea7
Thael <thaelzaruz@gmail.com>
2026-06-25 16:50:40 -0300

    Tabela Order, campo Instant, ISO 8601 - part 2

---
+++ src/main/java/com/thaeltech/course/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.thaeltech.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.thaeltech.course.entities.Order;
+
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+	
+}
