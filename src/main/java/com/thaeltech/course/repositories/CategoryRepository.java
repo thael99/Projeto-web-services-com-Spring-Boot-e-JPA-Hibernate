dc36f771f03d9959645cb5ece9f901aacff2d05d
Thael <thaelzaruz@gmail.com>
2026-06-26 20:34:23 -0300

    Category

---
+++ src/main/java/com/thaeltech/course/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.thaeltech.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.thaeltech.course.entities.Category;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+	
+}
