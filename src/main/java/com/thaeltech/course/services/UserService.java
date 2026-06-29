ad380eb79b42c65de2709ede5c0775d4f15c2862
Thael <thaelzaruz@gmail.com>
2026-06-29 02:22:01 -0300

    Exception handling - update

--- src/main/java/com/thaeltech/course/services/UserService.java
+++ src/main/java/com/thaeltech/course/services/UserService.java
@@ -5,13 +5,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.thaeltech.course.entities.User;
 import com.thaeltech.course.repositories.UserRepository;
 import com.thaeltech.course.services.exceptions.DatabaseException;
 import com.thaeltech.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -46,9 +47,13 @@ public class UserService {
 	}
 	
 	public User update (Long id, User obj) {
+		try{
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 		
 	}
 
