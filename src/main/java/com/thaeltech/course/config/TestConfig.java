672b5c7dc6a75ec9fca5a1269687d7a398e956df
Thael <thaelzaruz@gmail.com>
2026-06-28 22:33:29 -0300

    Payment, one to one association

--- src/main/java/com/thaeltech/course/config/TestConfig.java
+++ src/main/java/com/thaeltech/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.thaeltech.course.entities.Category;
 import com.thaeltech.course.entities.Order;
 import com.thaeltech.course.entities.OrderItem;
+import com.thaeltech.course.entities.Payment;
 import com.thaeltech.course.entities.Product;
 import com.thaeltech.course.entities.User;
 import com.thaeltech.course.entities.enums.OrderStatus;
@@ -41,8 +42,8 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.CANCELED, u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1); 
 		
 		Category cat1 = new Category(null, "Electronics");
@@ -70,14 +71,16 @@ public class TestConfig implements CommandLineRunner{
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
-		OrderItem oi1 = new OrderItem(o1, p2, 2, p2.getPrice());
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
 		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
-		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.saveAll(Arrays.asList(o1));
 	}
 	
 	
--- src/main/java/com/thaeltech/course/entities/Order.java
+++ src/main/java/com/thaeltech/course/entities/Order.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.thaeltech.course.entities.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,6 +16,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -36,7 +38,9 @@ public class Order implements Serializable {
 	private User client;
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	
+	private Payment payment;
 	
 	public Order() {	
 	}
@@ -76,6 +80,12 @@ public class Order implements Serializable {
 	}
 	public Set<OrderItem> getItems(){
 		return items;
+	}
+	public void setPayment (Payment payment) {
+		this.payment = payment;
+	}
+	public Payment getPayment () {
+		return payment;
 	}
 	
 	@Override
